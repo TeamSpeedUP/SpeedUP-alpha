4a1a0980644f0cb0aa6963132b7e312297fe864e
TeamSpeedUP <37194011+TeamSpeedUP@users.noreply.github.com>
2018-03-10 20:02:30 -0800

    Remove Old Files

--- Installerv3/WinVersionsSetups/Win10.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo "Welcome to the windows 10 version of SpeedUP-Alpha"
-echo "We need your support to continue this project, You can help us with it by Sharing issues on our github page!"
-pause
--- Installerv3/WinVersionsSetups/Win81.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo "Welcome to the windows 8.1 version of SpeedUP-Alpha"
-echo "We need your support to continue this project, You can help us with it by Sharing issues on our github page!"
-pause
